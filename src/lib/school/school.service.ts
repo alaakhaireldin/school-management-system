0d753687f98710ea8d6cd5601f4a7a10d7db54e0
alaakhaireldin <alaa.khaireldin@gmail.com>
2022-08-01 21:42:28 +0200

    writing the createSchool method

--- src/lib/school/school.service.ts
+++ src/lib/school/school.service.ts
@@ -1,6 +1,13 @@
+import moment = require('moment')
 import { v4 as uuid } from 'uuid'
 
 import { client } from '../db/db.service'
+
+interface SchoolDbModel {
+  schoolName: string
+  region?: string
+  createdAt: number
+}
 class SchoolService {
   public getSchool = async (id: string) => {
     const params = {
@@ -12,6 +19,24 @@ class SchoolService {
     const data = await client.get(params).promise()
     if (!data.Item) return
     return data.Item
+  }
+  private generateId = () => {
+    return uuid()
+  }
+
+  public createSchool = async ({ schoolName }: Pick<SchoolDbModel, 'schoolName'>) => {
+    const date = new Date().getTime()
+    const schoolId = this.generateId()
+    const params = {
+      TableName: 'items',
+      Item: {
+        id: schoolId,
+        schoolName,
+        createdAt: date,
+      },
+    }
+    const data = await client.put(params).promise()
+    return schoolId
   }
 }
 
