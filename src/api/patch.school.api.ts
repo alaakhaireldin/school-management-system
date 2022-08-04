5cd56523218670eaa8ab9a5f3e6641fda5ead1f9
alaakhaireldin <alaa.khaireldin@gmail.com>
2022-08-04 13:45:27 +0200

    adding joi validation to patchShool api

--- src/api/patch.school.api.ts
+++ src/api/patch.school.api.ts
@@ -1,7 +1,16 @@
 import { APIGatewayEvent, Context } from 'aws-lambda'
+import * as Joi from 'joi'
 
 import { SchoolDbModel, schoolService } from '../lib/school/school.service'
 import { isObjectEmpty } from '../lib/util/object.util'
+
+const schema = Joi.object().keys({
+  schoolName: Joi.string().min(3).max(30),
+  region: Joi.string().min(3).max(30),
+  schoolType: Joi.object().keys({
+    id: Joi.string().valid('private', 'public').required(),
+  }),
+})
 export const handler = async (event: APIGatewayEvent, context: Context) => {
   // console.log({ event, context })
   const schoolId = event.pathParameters!.id!
@@ -9,12 +18,18 @@ export const handler = async (event: APIGatewayEvent, context: Context) => {
   if (!body || isObjectEmpty(body)) {
     return { statusCode: 400, body: JSON.stringify({ message: `Must attach schools details to the request` }) }
   }
-  console.log(body)
+  // console.log(body)
+  const result = schema.validate(body)
+  if (result.error) {
+    return { statusCode: 404, body: JSON.stringify({ message: `${result.error.message}` }) }
+  } else {
+    console.log(result)
+  }
   await schoolService.updateSchool(schoolId, body)
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'School Updated',
+      message: 'Update done',
     }),
   }
 }
